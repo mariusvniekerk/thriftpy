8d336834e7dc91063ebea099f2e9e8802d6f8016
mariusvniekerk <marius.v.niekerk@gmail.com>
2015-03-13 15:21:04 -0400

    Added license

--- thriftpy/transport/sasl/cysasl.pyx
+++ thriftpy/transport/sasl/cysasl.pyx
@@ -1,3 +1,25 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+""" SASL transports for Thrift. """
+
+# Copied from the Impala repo
+
+
 from libc.stdlib cimport malloc, free
 from libc.string cimport memcpy
 from libc.stdint cimport int32_t, uint8_t
